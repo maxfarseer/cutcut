12f369451501d696d242c0cbdea01a714b50a97a
Max P <maxfarseer@gmail.com>
2019-12-01 20:05:07 +0100

    modal step add image works, next - crop

--- src/AddImg.elm
+++ src/AddImg.elm
@@ -1,35 +1,75 @@
-module AddImg exposing (Model, Msg(..), init, update, view)
+port module AddImg exposing (Model, Msg(..), init, update, view)
 
 import Accessibility.Modal as Modal
 import Css exposing (..)
 import Dict exposing (Dict)
+import File exposing (File)
 import Html.Styled exposing (..)
-import Html.Styled.Attributes exposing (class, css, id, multiple, name, src, type_)
+import Html.Styled.Attributes exposing (class, css, disabled, id, multiple, name, src, type_)
 import Html.Styled.Events exposing (on, onClick)
+import Http as Http
+import Json.Decode as D
 
 
 type alias Model =
     { modal : Dict Int Modal.Model
     , step : StepModel
+    , uploadStatus : UploadStatus
     }
+
+
+type alias Base64 =
+    String
+
+
+type UploadStatus
+    = NotAsked
+    | Loading
+    | Errored Http.Error
 
 
 type StepModel
     = AddImgStep
-    | CropImgStep
-    | EraseImtStep
+    | CropImgStep Base64
+    | EraseImgStep
 
 
 type Msg
     = ClickedAddImg
     | ModalMsg Int Modal.Msg
+    | GotFiles (List File)
+    | GotRemoveBgResponse (Result Http.Error Base64)
 
 
 init : Model
 init =
     { modal = Dict.fromList [ ( 0, Modal.init ) ]
     , step = AddImgStep
+    , uploadStatus = NotAsked
     }
+
+
+
+-- CONST
+
+
+removeBgApiUrl : String
+removeBgApiUrl =
+    "https://api.remove.bg/v1.0"
+
+
+
+-- DECODERS
+
+
+filesDecoder : D.Decoder (List File)
+filesDecoder =
+    D.at [ "target", "files" ] (D.list File.decoder)
+
+
+fileBgDecoder : D.Decoder Base64
+fileBgDecoder =
+    D.field "data" (D.field "result_b64" D.string)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -55,6 +95,39 @@ update msg model =
                 Nothing ->
                     ( model, Cmd.none )
 
+        GotFiles files ->
+            case List.head files of
+                Nothing ->
+                    ( model, Cmd.none )
+
+                Just file ->
+                    ( { model | uploadStatus = Loading }
+                    , Http.request
+                        { url = removeBgApiUrl ++ "/removebg"
+                        , headers =
+                            [ Http.header "X-Api-Key" "Ge5HqmTYvcD1UzadQ7MPVPVi"
+                            , Http.header "Accept" "application/json"
+                            ]
+                        , method = "POST"
+                        , timeout = Nothing
+                        , tracker = Nothing
+                        , body = Http.multipartBody [ Http.filePart "image_file" file ]
+                        , expect = Http.expectJson GotRemoveBgResponse fileBgDecoder
+                        }
+                    )
+
+        GotRemoveBgResponse result ->
+            case result of
+                Err err ->
+                    ( { model | uploadStatus = Errored err }, Cmd.none )
+
+                Ok response ->
+                    ( { model
+                        | step = CropImgStep response
+                      }
+                    , sendDataToJs response
+                    )
+
 
 view : Model -> Html Msg
 view model =
@@ -63,12 +136,12 @@ view model =
             section []
                 [ viewModalOpener 0
                 , Modal.view (ModalMsg 0)
-                    "Add image"
-                    [ Modal.overlayColor (rgba 128 0 128 0.7)
+                    ""
+                    [ Modal.overlayColor (rgba 0 0 0 0.7)
                     , Modal.onlyFocusableElementView
                         (\onlyFocusableElement ->
-                            div [ css [ displayFlex, justifyContent spaceBetween ] ]
-                                [ text "Modal content"
+                            div []
+                                [ viewModalBody model
                                 , button
                                     (onClick (ModalMsg 0 Modal.close)
                                         :: onlyFocusableElement
@@ -95,3 +168,77 @@ viewModalOpener uniqueId =
         , onClick (ModalMsg uniqueId (Modal.open elementId))
         ]
         [ text "Add image" ]
+
+
+viewModalBody : Model -> Html Msg
+viewModalBody model =
+    case model.step of
+        AddImgStep ->
+            viewAddImgStep model.uploadStatus
+
+        CropImgStep base64 ->
+            viewCropImage base64
+
+        EraseImgStep ->
+            div []
+                [ text "Erase image" ]
+
+
+viewAddImgStep : UploadStatus -> Html Msg
+viewAddImgStep uploadStatus =
+    case uploadStatus of
+        NotAsked ->
+            div []
+                [ viewUploadFileBtn "Remove background" True
+                , viewUploadFileBtn "Upload as is (INACTIVE)" False
+                ]
+
+        Loading ->
+            div []
+                [ text "Loading ..." ]
+
+        Errored err ->
+            div []
+                [ text "Error!" ]
+
+
+viewCropImage : Base64 -> Html Msg
+viewCropImage base64 =
+    div []
+        [ h2 [] [ text "Crop image" ]
+        , img [ src <| "data:image/png;base64, " ++ base64 ] []
+        ]
+
+
+viewUploadFileBtn : String -> Bool -> Html Msg
+viewUploadFileBtn btnLabel isActive =
+    form []
+        [ div [ class "file" ]
+            [ label [ class "file-label" ]
+                [ input
+                    [ class "file-input"
+                    , name "resume"
+                    , type_ "file"
+                    , multiple False
+                    , on "change" (D.map GotFiles filesDecoder)
+                    , Html.Styled.Attributes.disabled (not isActive)
+                    ]
+                    []
+                , span [ class "file-cta" ]
+                    [ span [ class "file-icon" ]
+                        [ i [ class "fas fa-upload" ]
+                            []
+                        ]
+                    , span [ class "file-label" ]
+                        [ text <| btnLabel ]
+                    ]
+                ]
+            ]
+        ]
+
+
+
+-- PORTS
+
+
+port sendDataToJs : String -> Cmd msg
--- src/Main.elm
+++ src/Main.elm
@@ -1,42 +1,24 @@
-port module Main exposing (main)
+module Main exposing (main)
 
 import AddImg
 import Browser
 import Css exposing (..)
-import File exposing (File)
 import Html.Styled exposing (..)
 import Html.Styled.Attributes exposing (class, css, id, multiple, name, src, type_)
 import Html.Styled.Events exposing (on, onClick)
-import Http as Http
-import Json.Decode as D
 
 
 type alias Model =
-    { list : List Base64
-    , uploadStatus : UploadStatus
-    , addImg : AddImg.Model
+    { addImg : AddImg.Model
     }
-
-
-type UploadStatus
-    = NotAsked
-    | Loading
-    | Loaded
-    | Errored Http.Error
 
 
 initialModel : ( Model, Cmd Msg )
 initialModel =
-    ( { list = []
-      , uploadStatus = NotAsked
-      , addImg = AddImg.init
+    ( { addImg = AddImg.init
       }
     , Cmd.none
     )
-
-
-type alias Base64 =
-    String
 
 
 type ToJSmsg
@@ -44,20 +26,13 @@ type ToJSmsg
 
 
 type Msg
-    = GotFiles (List File)
-    | GotRemoveBgResponse (Result Http.Error Base64)
-    | ToJS ToJSmsg
+    = ToJS ToJSmsg
     | FromAddImg AddImg.Msg
 
 
 customCanvas : List (Attribute a) -> List (Html a) -> Html a
 customCanvas attributes children =
     node "custom-canvas" attributes children
-
-
-removeBgApiUrl : String
-removeBgApiUrl =
-    "https://api.remove.bg/v1.0"
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -70,75 +45,21 @@ update msg model =
             in
             ( { model | addImg = updatedAddImg }, addImgCmd |> Cmd.map FromAddImg )
 
-        GotFiles files ->
-            case List.head files of
-                Nothing ->
-                    ( model, Cmd.none )
-
-                Just file ->
-                    ( { model | uploadStatus = Loading }
-                    , Http.request
-                        { url = removeBgApiUrl ++ "/removebg"
-                        , headers =
-                            [ Http.header "X-Api-Key" "Ge5HqmTYvcD1UzadQ7MPVPVi"
-                            , Http.header "Accept" "application/json"
-                            ]
-                        , method = "POST"
-                        , timeout = Nothing
-                        , tracker = Nothing
-                        , body = Http.multipartBody [ Http.filePart "image_file" file ]
-                        , expect = Http.expectJson GotRemoveBgResponse fileBgDecoder
-                        }
-                    )
-
-        GotRemoveBgResponse result ->
-            case result of
-                Err err ->
-                    ( { model | uploadStatus = Errored err }, Cmd.none )
-
-                Ok response ->
-                    ( { model
-                        | list = response :: model.list
-                        , uploadStatus = Loaded
-                      }
-                    , sendDataToJs response
-                    )
-
         ToJS msgToJs ->
             case msgToJs of
                 DrawSquare ->
-                    ( model, sendDataToJs "iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAARUlEQVR42u3PMREAAAgEoLd/AtNqBlcPGlDJdB4oEREREREREREREREREREREREREREREREREREREREREREREREREZGLBddNT+MQpgCuAAAAAElFTkSuQmCC" )
+                    ( model, Cmd.none )
 
 
-port sendDataToJs : String -> Cmd msg
+
+--                DrawSquare ->
+--                    ( model, sendDataToJs "iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAARUlEQVR42u3PMREAAAgEoLd/AtNqBlcPGlDJdB4oEREREREREREREREREREREREREREREREREREREREREREREREREZGLBddNT+MQpgCuAAAAAElFTkSuQmCC" )
 
 
 view : Model -> Html Msg
 view model =
     div []
         [ h2 [] [ text "CutCut" ]
-        , form []
-            [ div [ class "file" ]
-                [ label [ class "file-label" ]
-                    [ input
-                        [ class "file-input"
-                        , name "resume"
-                        , type_ "file"
-                        , multiple False
-                        , on "change" (D.map GotFiles filesDecoder)
-                        ]
-                        []
-                    , span [ class "file-cta" ]
-                        [ span [ class "file-icon" ]
-                            [ i [ class "fas fa-upload" ]
-                                []
-                            ]
-                        , span [ class "file-label" ]
-                            [ text "Choose a file… " ]
-                        ]
-                    ]
-                ]
-            ]
         , renderCustomCanvas
         , button [ onClick (ToJS DrawSquare) ] [ text "Draw square" ]
         , map FromAddImg (AddImg.view model.addImg)
@@ -171,13 +92,3 @@ main =
 subscriptions : Model -> Sub Msg
 subscriptions _ =
     Sub.none
-
-
-filesDecoder : D.Decoder (List File)
-filesDecoder =
-    D.at [ "target", "files" ] (D.list File.decoder)
-
-
-fileBgDecoder : D.Decoder Base64
-fileBgDecoder =
-    D.field "data" (D.field "result_b64" D.string)
